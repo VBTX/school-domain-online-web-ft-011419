4a68ba42f89620d6588b96f6a14b1304fec61f49
Victoria B <viktorija.bourassa@gmail.com>
2019-01-21 23:24:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,13 +6,4 @@ class School
     @roster = {}
   end
 
-  def add_student(name, grade)
-    School.roster[grade] = []
-    School.roster[grade] << name
-  end
-
-  def grade(grade)
-    School.roster[grade]
-    
-  end
 end
