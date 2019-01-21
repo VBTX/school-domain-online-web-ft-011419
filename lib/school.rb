16d93c4a569bd5e1e68f785d00f2778658372a49
Victoria B <viktorija.bourassa@gmail.com>
2019-01-21 23:24:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  attr_reader :name
+
+  def initialize(name, roster)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    School.roster[grade] = []
+    School.roster[grade] << name
+  end
+
+  def grade(grade)
+    School.roster[grade]
+    
+  end
+end
